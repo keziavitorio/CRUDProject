bb05826346121ec5556cb1b6b254d70475e6f155
kezia.vitorio@bndes.gov.br <kezia.vitorio@bndes.gov.br>
2023-10-05 12:57:59 -0300

    inicio codigo localStorage

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ const openModal = () =>
 const closeModal = () =>
   document.getElementById("modal").classList.remove("active");
 
+const templateClient = {
+  nome: "Kez",
+  email: "keziavitorio@gmail.com",
+  celular: "21990176868",
+  cidade: "Nova Iguaçu",
+}
+
+const createClient = (client) => {
+  localStorage.setItem("teste", "teste para o site")
+}
+
+
+//eventos  
+
 document
   .getElementById("cadastrarCliente")
   .addEventListener("click", openModal);
